011664886a040cd47bd2423a76948beb0fc869fa
Arnab Sahoo <arnabsahoo10@gmail.com>
2022-08-12 12:30:39 +0530

    _app.js with error boundry and server error handeling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Error from "next/error";
+import ErrorBoundary from "../Components/ErrorBoundry/ErrorBoundry";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    if (pageProps.error.code) {
+        return <Error statusCode={pageProps.error.code} title={pageProps.error.message} />;
+    }
+    return (
+        <ErrorBoundary>
+            <Component {...pageProps} />
+        </ErrorBoundary>
+    );
 }
 
-export default MyApp
+export default MyApp;
