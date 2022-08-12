d113155e043bee3212d6dc6197b5d9a68f821b0a
Arnab Sahoo <arnabsahoo10@gmail.com>
2022-08-12 12:22:22 +0530

    navbar added

---
+++ Components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledNavbar } from "./navbar.styled";
+
+export default function Navbar() {
+    return (
+        <StyledNavbar>
+            <img src="https://fampay.in/assets/images/icons/common/headerGreyLogoIcon.svg" alt="logo" />
+        </StyledNavbar>
+    );
+}
---
+++ Components/Navbar/navbar.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledNavbar = styled.div`
+    background-color: #ffffff;
+    height: 65px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        display: inline-block;
+        height: 33px;
+    }
+`;
