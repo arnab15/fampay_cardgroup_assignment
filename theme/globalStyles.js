80b9a60a0a8375e67c2f5997d6676af356ae7cea
Arnab Sahoo <arnabsahoo10@gmail.com>
2022-08-12 12:28:04 +0530

    global style for styled component

---
+++ theme/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #F7F6F3;
+    font-family: Roboto;
+    overflow-x: hidden;
+    width: 100vw;
+  }
+`;
+
+export default GlobalStyle;
