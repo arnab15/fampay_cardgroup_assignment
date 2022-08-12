ca2836a851eb82b0cf86bf085ce93c05b9bd81d4
Arnab Sahoo <arnabsahoo10@gmail.com>
2022-08-12 12:47:39 +0530

    updated null check

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import ErrorBoundary from "../Components/ErrorBoundry/ErrorBoundry";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    if (pageProps.error.code) {
+    if (pageProps?.error?.code) {
         return <Error statusCode={pageProps.error.code} title={pageProps.error.message} />;
     }
     return (
